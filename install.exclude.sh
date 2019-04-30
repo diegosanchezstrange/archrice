97364cffe51a15d1fc883b809e01e4cc2f4775d9
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2019-04-30 16:53:50 +0200

    Installer now installs some useful programs

--- install.exclude.sh
+++ install.exclude.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
-
+echo -e "\e[32m==> Installing dotfiles...\e[0m"
 for file in $( ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md' ); do
-	ln -sv "$PWD/$file" "$HOME" 2> /dev/null || echo -e "$file already exists."
+	ln -sv "$PWD/$file" "$HOME" 2> /dev/null || echo -e "\e[31m$file already exists.\e[0m"
 done
 
+option="x"
+while [[ ! $option =~ [nNsS] ]]; do 
+	echo -en "\e[33m==> Do you want to install i3 realted programs? \e[0m"
+	read option
+
+	if [[ $option =~ [sS] ]]; then
+		echo -e "\e[32m==> Installing i3 related programs\e[0m"
+		sudo pacman -S i3-gaps i3blocks i3lock i3status rofi ranger
+	elif [[ $option =~ [nN] ]]; then
+		echo -e "\e[31m==> Ok\e[0m"
+	else 
+		echo -e "\e[31m==> I didn't get that.\e[0m"
+	fi
+done
+
+option="x"
+while [[ ! $option =~ [nNsS] ]]; do 
+	echo -en "\e[33m==> Do you want to install some useful staff?\e[0m" 
+	read option
+
+	if [[ $option =~ [sS] ]]; then
+		echo -e "\e[32m==> Installing some useful staff\e[0m"
+		sudo pacman -S yay
+		yay -S firefox spotify steam telegram-desktop
+	elif [[ $option =~ [nN] ]]; then
+		echo -e "\e[31m==> Ok\e[0m"
+	else 
+		echo -e "\e[31m==> I didn't get that.\e[0m"
+	fi
+done
